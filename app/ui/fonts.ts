74fac40cb4f7e020482618af037535a2a0898e6f
Mikhail Shvets <shvetsm123@gmail.com>
2024-03-21 11:33:40 +0200

    First commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google';
+import {NextFont} from "next/dist/compiled/@next/font";
+import {undefined} from "zod";
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({weight: ['400'], subsets: ['latin'] });
